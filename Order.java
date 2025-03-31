ed08ed2e982be18fdc88557cff8b17b68119e424
haralekishanrao@gmail.com <kishanharale>
2025-03-31 15:09:46 -0500

    commit Order class

---
+++ Order.java
@@ -0,0 +1,12 @@
+public class Order{
+    private String orderName;
+
+    public Order(String orderName){
+        this.orderName=orderName;
+    }
+
+    public String getOrder(){
+        return orderName;
+    }
+
+}
